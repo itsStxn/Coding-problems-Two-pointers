881c3631511273a947c4fd7219ec7f31928d2900
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-22 20:48:18 +0200

    Solved - C# - Two Sum II - Input array is sorted

---
+++ Medium/Two Sum II - Input Array Is Sorted/Program.cs
@@ -0,0 +1,5 @@
+﻿using Two_Sum_II___Input_Array_Is_Sorted;
+
+var task = new Solution();
+var result = task.TwoSum([1,3,4,4], 8);
+Console.WriteLine(string.Join(", ", result));
---
+++ Medium/Two Sum II - Input Array Is Sorted/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace Two_Sum_II___Input_Array_Is_Sorted;
+
+public class Solution {
+	public int[] TwoSum(int[] numbers, int target) {
+		int left = 0, right = numbers.Length - 1;
+
+		while (true) {
+			int sum = numbers[left] + numbers[right];
+
+			if (target > sum) left++;
+			else if (target < sum) right--;
+			else return [left + 1, right + 1];
+		}
+	}
+}
