fa5f2e9038649580f08ddc69d6e8bad5fa281050
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-07-26 23:16:17 +0200

    In progress - C# - 3Sum

---
+++ Medium/3Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿using _3Sum;
+
+var task = new Solution();
+var result = task.ThreeSum([-1, 0, 1, 2, -1, -4]);
+
+string output = string.Empty;
+foreach (var list in result) {
+	output += $"[{string.Join(", ", list)}]\n";
+}
+Console.WriteLine(output);
---
+++ Medium/3Sum/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace _3Sum;
+
+public class Solution {
+	public IList<IList<int>> ThreeSum(int[] nums) {
+		Array.Sort(nums);
+		
+		IList<IList<int>> result = [];
+		HashSet<string> seen = [];
+		
+		for (int i = 0; i < nums.Length - 2; i++) {
+			if (i > 0 && nums[i] == nums[i - 1]) continue;
+			TwoSum(nums, i, seen, result);
+		}
+
+		return result;
+	}
+	public void TwoSum(int[] nums, int targetIndex, HashSet<string> seen, IList<IList<int>> result) {
+		int target = -nums[targetIndex],
+		right = nums.Length - 1,
+		left = targetIndex + 1;
+
+		while (left < right) {
+			int sum = nums[left] + nums[right];
+
+			if (target > sum)
+				left++;
+			else if (target < sum)
+				right--;
+			else {
+				List<int> triplet = [nums[targetIndex], nums[left++], nums[right--]];
+				if (seen.Add(string.Join(",", triplet))) {
+					result.Add(triplet);
+				}
+
+				while (left < right && nums[left] == nums[left - 1]) left++;
+				while (left < right && nums[right] == nums[right + 1]) right--;
+			}
+		}
+	}
+}
