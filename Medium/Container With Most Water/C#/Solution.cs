10c1817f784fe579789906c2eee6ef882464134e
Stanley Sandjo <r0878901@student.thomasmore.be>
2024-09-23 22:33:52 +0200

    In progress 1.1.0 - C# - Container With Most Water

--- Medium/Container With Most Water/C#/Solution.cs
+++ Medium/Container With Most Water/C#/Solution.cs
@@ -2,6 +2,25 @@ namespace Container_With_Most_Water;
 
 public class Solution {
 	public int MaxArea(int[] height) {
-		return default;
+		int n = height.Length - 1,
+		right = n, 
+		left = 0;
+
+		for (int r = 0, l = n; r <= n; r++, l--) {
+			int h = height[r],
+			areaRight = right * height[right],
+			areaLeft = (n - left) * height[left];
+
+			left = l * h > areaLeft ? r : left;
+			right = r * h >= areaRight ? r : right;
+		}
+
+		Console.WriteLine($"{left}, {right}");
+		Console.WriteLine($"{Math.Min(height[left], height[right])}");
+
+		return GetArea(height, left, right);
+	}
+	private int GetArea(int[] height, int left, int right) {
+		return (right - left) * Math.Min(height[left], height[right]);
 	}
 }
